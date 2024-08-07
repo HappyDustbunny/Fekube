01375192db9bb58396c4dc9b7c1c022b13ecf6dd
HappyDustbunny <madshorn@emailcorner.net>
2024-08-07 12:23:25 +0200

    Add print sheat for QR codes, format Story.txt and make a QR code generator function

--- fekube.js
+++ fekube.js
@@ -1,3 +1,26 @@
+
+function generateQRcode(text) {
+    let responseQRcode = new QRCodeStyling({
+        width: 300,
+        height: 300,
+        type: "svg",
+        data: text,
+        image: "qr-codes/fairy.png",
+        dotsOptions: {
+            color: "000",
+            type: "squrare"
+        },
+        imageOptions:{
+            crossOrigin: "anonymous",
+            margin: 20
+        }
+    });
+
+    return responseQRcode;
+}
+
+generateQRcode("blarp").append(document.getElementById("canvas"));
+
 // QR-code reader
 function onScanSuccess(decodedText, decodedResult) {
     // handle the scanned code as you like, for example:
@@ -20,24 +43,24 @@ document.getElementById('firstButton').addEventListener('click', function() {
     html5QrcodeScanner.render(onScanSuccess, onScanFailure);
 });
 
-// QR-code generator
-const qrCode = new QRCodeStyling({
-    width: 300,
-    height: 300,
-    type: "svg",
-    data: "abcdefghi jklmnopqr stuvwxyz 1234567890!#¤%&/()=?~^*<>", // Max 256 characters or the QR pixels will be too small. May have to encode information in bits
-    image: "qr-codes/fairy.png",
-    dotsOptions: {
-        color: "#000",
-        type: "square"
-    },
-    backgroundOptions: {
-        color: "#fff",
-    },
-    imageOptions: {
-        crossOrigin: "anonymous",
-        margin: 20
-    }
-});
+// // QR-code generator
+// const qrCode = new QRCodeStyling({
+//     width: 300,
+//     height: 300,
+//     type: "svg",
+//     data: "abcdefghi jklmnopqr stuvwxyz 1234567890!#¤%&/()=?~^*<>", // Max 256 characters or the QR pixels will be too small. May have to encode information in bits
+//     image: "qr-codes/fairy.png",
+//     dotsOptions: {
+//         color: "#000",
+//         type: "square"
+//     },
+//     backgroundOptions: {
+//         color: "#fff",
+//     },
+//     imageOptions: {
+//         crossOrigin: "anonymous",
+//         margin: 20
+//     }
+// });
 
-qrCode.append(document.getElementById("canvas"));
+// qrCode.append(document.getElementById("canvas"));
