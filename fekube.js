c00d187122faf239a66dc85f0054e8031c0781f2
HappyDustbunny <madshorn@emailcorner.net>
2024-08-08 13:59:51 +0200

    Add better QR reader. Slightly better...

--- fekube.js
+++ fekube.js
@@ -1,4 +1,5 @@
 
+// QR-code generator
 function generateQRcode(text) {
     let responseQRcode = new QRCodeStyling({
         width: 300,
@@ -21,46 +22,50 @@ function generateQRcode(text) {
 
 generateQRcode("blarp").append(document.getElementById("canvas"));
 
+
 // QR-code reader
-function onScanSuccess(decodedText, decodedResult) {
-    // handle the scanned code as you like, for example:
-    console.log(`Code matched = ${decodedText}`, decodedResult);
-    let outputDiv = document.getElementById('output');
-    let content = document.createTextNode(decodedText);
-    outputDiv.appendChild(content);
-}
+function grabQRcode() {
+    const html5Qrcode = new Html5Qrcode("reader");
+    const config = {fps: 10, qrbox: {width: 300, height: 300}};
+    const qrCodeHasBeenRead = (decodedText, decodedResult) => {
+        console.log(`Code matched = ${decodedText}`, decodedResult);
+        let outputDiv = document.getElementById('output');
+        let content = document.createTextNode(decodedText);
+        outputDiv.appendChild(content);
 
-function onScanFailure(error) {
-    // handle scan failure, usually better to ignore and keep scanning.
-    // for example:
-    console.warn(`Code scan error = ${error}`);
+        html5Qrcode.stop().then((ignore) => {
+            console.log('QR scanning stopped');
+        }).catch((err) => {
+            console.log('QR scanning did not stop for some reason');
+        })
+    }
+    
+    return html5Qrcode.start({facingMode: "environment"}, config, qrCodeHasBeenRead);
 }
-
 
 document.getElementById('firstButton').addEventListener('click', function() {
-    let html5QrcodeScanner = new Html5QrcodeScanner(
-        "reader", { fps: 10, qrbox: {width: 250, height: 250} }, /* verbose= */ false);
-    html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+    decodedText = grabQRcode();
+    console.log(`Decoded text = ${decodedText}`);
 });
 
-// // QR-code generator
-// const qrCode = new QRCodeStyling({
-//     width: 300,
-//     height: 300,
-//     type: "svg",
-//     data: "abcdefghi jklmnopqr stuvwxyz 1234567890!#¤%&/()=?~^*<>", // Max 256 characters or the QR pixels will be too small. May have to encode information in bits
-//     image: "qr-codes/fairy.png",
-//     dotsOptions: {
-//         color: "#000",
-//         type: "square"
-//     },
-//     backgroundOptions: {
-//         color: "#fff",
-//     },
-//     imageOptions: {
-//         crossOrigin: "anonymous",
-//         margin: 20
-//     }
-// });
 
-// qrCode.append(document.getElementById("canvas"));
+// function onScanSuccess(decodedText, decodedResult) {
+//     // handle the scanned code as you like, for example:
+//     console.log(`Code matched = ${decodedText}`, decodedResult);
+//     let outputDiv = document.getElementById('output');
+//     let content = document.createTextNode(decodedText);
+//     outputDiv.appendChild(content);
+// }
+
+// function onScanFailure(error) {
+//     // handle scan failure, usually better to ignore and keep scanning.
+//     // for example:
+//     console.warn(`Code scan error = ${error}`);
+// }
+
+
+// document.getElementById('firstButton').addEventListener('click', function() {
+//     let html5QrcodeScanner = new Html5QrcodeScanner(
+//         "reader", { fps: 10, qrbox: {width: 250, height: 250} }, /* verbose= */ false);
+//     html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+// });
